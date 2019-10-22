d4a435e484bfc9f2f4b981245c8d38d04b56f05e
Ugaitz Etxebarria Iñarra <etxeba17@gmail.com>
2019-10-23 00:10:15 +0200

    prueba hello world

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
-const Alexa = require('alexa-sdk');
+var express = require('express');
+var app = express();
 
-// Replace with your app ID (OPTIONAL).
-// You can find this value at the top of your skill's page on http://developer.amazon.com.
-// Make sure to enclose your value in quotes,
-// like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
-const APP_ID = 'amzn1.ask.skill.b3521eab-2983-42cf-8f08-65e4155ac90d';
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-const handlers = {
-  'LaunchRequest': function () {
-    this.emit('WhatDidILearnIntent');
-  },
-  'WhatDidILearnIntent': function () {
-    const say = 'Congratulations! You have learned how to build your first Alexa skill.';
-
-    this.response.speak(say);
-    this.emit(':responseReady');
-  },
-  'AMAZON.HelpIntent': function () {
-    const say = 'You can say what did I learn, or, you can say exit... How can I help you?';
-
-    this.response.speak(say).listen(say);
-    this.emit(':responseReady');
-  },
-  'AMAZON.CancelIntent': function () {
-    this.response.speak('Bye!');
-    this.emit(':responseReady');
-  },
-  'AMAZON.StopIntent': function () {
-    this.response.speak('Bye!');
-    this.emit(':responseReady');
-  }
-};
-
-exports.handler = function (event, context, callback) {
-  const alexa = Alexa.handler(event, context, callback);
-  alexa.APP_ID = APP_ID;
-  alexa.registerHandlers(handlers);
-  alexa.execute();
-};
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
