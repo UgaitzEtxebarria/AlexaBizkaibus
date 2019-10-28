1a580f8d4944a36b8bd9abe1168d234faeffdadd
Ugaitz <etxeba17@gmail.com>
2019-10-28 18:43:59 +0100

    Hello world in alexa skill

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const Alexa = require('alexa-sdk');
-
-// Replace with your app ID (OPTIONAL).
-// You can find this value at the top of your skill's page on http://developer.amazon.com.
-// Make sure to enclose your value in quotes,
-// like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
-const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
-
-const handlers = {
-  'LaunchRequest': function () {
-    this.emit('WhatDidILearnIntent');
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
-  'WhatDidILearnIntent': function () {
-    const say = 'Congratulations! You have learned how to build your first Alexa skill.';
-
-    this.response.speak(say);
-    this.emit(':responseReady');
-  },
-  'AMAZON.HelpIntent': function () {
-    const say = 'You can say what did I learn, or, you can say exit... How can I help you?';
-
-    this.response.speak(say).listen(say);
-    this.emit(':responseReady');
-  },
-  'AMAZON.CancelIntent': function () {
-    this.response.speak('Bye!');
-    this.emit(':responseReady');
-  },
-  'AMAZON.StopIntent': function () {
-    this.response.speak('Bye!');
-    this.emit(':responseReady');
+  handle(handlerInput) {
+      const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+return handlerInput.responseBuilder
+          .speak(speechText)
+          .reprompt(speechText)
+          .withSimpleCard('Hello World', speechText)
+          .getResponse();
   }
 };
 
-exports.handler = function (event, context, callback) {
-  const alexa = Alexa.handler(event, context, callback);
-  alexa.APP_ID = APP_ID;
-  alexa.registerHandlers(handlers);
-  alexa.execute();
+const HelloWorldIntentHandler = {
+  canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+          && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+  },
+  handle(handlerInput) {
+      const speechText = 'Hello World!';
+return handlerInput.responseBuilder
+          .speak(speechText)
+          .withSimpleCard('Hello World', speechText)
+          .getResponse();
+  }
 };
+
+
+const proximo_autobus = {
+  canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+          && handlerInput.requestEnvelope.request.intent.name === 'proximo_autobus';
+  },
+  handle(handlerInput) {
+      const speechText = 'Hello World!';
+return handlerInput.responseBuilder
+          .speak(speechText)
+          .withSimpleCard('Hello World', speechText)
+          .getResponse();
+  }
+};
+
