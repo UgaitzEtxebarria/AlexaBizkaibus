2302445ce640864291d8c3ec8b45e630d09539e1
Ugaitz <etxeba17@gmail.com>
2019-11-13 21:20:39 +0100

    Test for get headers

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 
 var AlexaAppServer = require('alexa-app-server');
  
-var instance = AlexaAppServer.start({
+var instance = new AlexaAppServer({
   server_root: __dirname,     // Path to root
   public_html: "public_html", // Static content
   app_dir: "apps",            // Location of alexa-app modules
   app_root: "/alexa/",        // Service root
   port: process.env.PORT || 8080                  // Port to use
 });
+
+server.start();
+server.express.use('/test', function(req, res) { res.send("OK"); });
