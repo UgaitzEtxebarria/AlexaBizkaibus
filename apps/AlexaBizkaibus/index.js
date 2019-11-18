673f49f372e04d3b741e4f696ad60aac8fd8d9d0
Ugaitz <etxeba17@gmail.com>
2019-11-18 22:37:10 +0100

    changed blank

--- apps/AlexaBizkaibus/index.js
+++ apps/AlexaBizkaibus/index.js
@@ -29,7 +29,7 @@ app.intent('proximo_autobus',
   },
   function (request, response) {
     //var number = request.slot('number');
-    var resource = "http://apli.bizkaia.net/APPS/DANOK/TQWS/TQ.ASMX/GetPasoParadaMobile_JSON?callback=%22%22&strLinea=A3612&strParada=0270";
+    var resource = "http://apli.bizkaia.net/APPS/DANOK/TQWS/TQ.ASMX/GetPasoParadaMobile_JSON?callback=''&strLinea=A3612&strParada=0270";
     https.get(resource, (resp) => {
       let data = '';
 
