c9d484f12d3fe9414a028de622824081518d958d
Ugaitz <etxeba17@gmail.com>
2019-11-20 22:05:17 +0100

    Changed http to https

--- apps/AlexaBizkaibus/index.js
+++ apps/AlexaBizkaibus/index.js
@@ -31,7 +31,7 @@ app.intent('proximo_autobus',
     //var number = request.slot('number');
     var url = 'http://apli.bizkaia.net/APPS/DANOK/TQWS/TQ.ASMX/GetPasoParadaMobile_JSON?callback=%22%22&strLinea=A3612&strParada=0270';
 
-  http.get(url, function(res){
+  https.get(url, function(res){
       var body = '';
   
       res.on('data', function(chunk){
