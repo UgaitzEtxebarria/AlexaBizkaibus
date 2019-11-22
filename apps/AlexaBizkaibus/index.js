7c3469ec55244a4e24face6acb20490bde21ed76
Ugaitz Etxebarria <uetxebarria@talenssys.com>
2019-11-22 12:20:20 +0100

    Is that a dix?

--- apps/AlexaBizkaibus/index.js
+++ apps/AlexaBizkaibus/index.js
@@ -152,7 +152,7 @@ app.intent('proximo_autobus',
 	.then(ok => {
       console.log("OK!: ");
 	  console.log(ok);
-	  var definitivo = processBody(ok);
+	  var definitivo = "Hola";//processBody(ok);
 	  console.log("Respuesta definitiva: ");
 	  console.log(definitivo);
 	  response.say(definitivo);
